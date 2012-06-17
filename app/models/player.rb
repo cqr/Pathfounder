f548efca0e4ae3dd37308c4c3dc5b7abe530aa1d
chrisrhoden <carhoden@gmail.com>
2012-06-17 15:13:11 -0400

    less magic

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,16 +1,17 @@
 class Player < ActiveRecord::Base
   belongs_to :game
-  has_many :base_stats do
+  has_many :base_stats, :auto_save => true do
     def [](short_name)
        where(stats:{short_name:short_name}).joins(:stat).first.try :value
     end
     def []=(short_name, value)
-      Player.transaction do
-        self[short_name].try :destroy
-        create stat:Stat.where(short_name:short_name).first, value: value
-      end
+      stat = Stat.where(short_name:short_name).first
+      base_stat = where(stat:stat).first
+      (base_stat || build(stat:stat)).value = value
     end
   end
   attr_accessible :name
+  belongs_to :race
+  belongs_to :player_class
 
 end
